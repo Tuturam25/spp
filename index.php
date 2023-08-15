351390e1acd2465b785ee0c062d7b7f5bd0d9aaa
tuturam <ahmadrama251005@gmail.com>
2023-08-15 13:46:14 +0700

    tambah index

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        <label for="user">username</label>
+        <input type="text" name="username" id="user">
+        <label for="pas">password</label>
+        <input type="password" name="pass" id="pas">
+    </form>
+</body>
+</html>
